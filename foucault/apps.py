c8b9946cef08746f00575eb85d69f9e435d38a5d
Lecrodez <shilkinartik04@gmail.com>
2025-02-14 21:55:22 +0500

    first commit

---
+++ foucault/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FoucaultConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "foucault"
---
+++ users/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    avatar = models.ImageField(upload_to="users/%Y/%m/%d", blank=True, null=True, verbose_name="Фотография")
+    roles = models.ForeignKey('Roles', on_delete=models.PROTECT, default=1)
+
+
+    def __str__(self):
+        return self.username
+
+
+class Roles(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+
+    def __str__(self):
+        return self.name
