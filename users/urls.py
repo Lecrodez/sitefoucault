0d53eeee244844074670c73bf385f6c09d941002
Lecrodez <shilkinartik04@gmail.com>
2025-02-16 18:15:21 +0500

    first commit

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+app_name = 'users'
+
 urlpatterns = [
-    path("users/", views.register, name="users"),
+    path("login/", views.register, name="login"),
 ]
