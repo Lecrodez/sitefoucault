158fe783d7e6894c8cd267f9605fdf345ff1fbfe
Lecrodez <shilkinartik04@gmail.com>
2025-02-18 20:10:07 +0500

    dump

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from users import views
@@ -8,3 +10,6 @@ urlpatterns = [
     path("login/", views.login_user, name="login"),
     path("logout/", views.logout_user, name="logout"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
