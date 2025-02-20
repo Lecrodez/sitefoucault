a4a9b55c8621bbd16247d3f0e6d6129215dff5b8
Lecrodez <shilkinartik04@gmail.com>
2025-02-20 16:31:30 +0500

    auth

--- users/forms.py
+++ users/forms.py
@@ -5,6 +5,14 @@ from django import forms
 from .models import User
 
 
+"""Проверяет, что строка содержит только русские буквы, пробелы и дефисы."""
+import re
+from django.core.exceptions import ValidationError
+
+def validate_russian_letters(value):
+    if not re.match(r'^[А-Яа-яЁё\s-]+$', value):
+        raise ValidationError('Введите только русские буквы, пробелы и дефисы.')
+
 class LoginUserForm(AuthenticationForm):
     username = forms.EmailField(label="E-mail",
                                widget=forms.TextInput(attrs={'class': 'form-input'}))
@@ -19,8 +27,19 @@ class LoginUserForm(AuthenticationForm):
 
 class RegisterUserForm(UserCreationForm):
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ('first_name', 'last_name', 'roles', 'email', 'password1', 'password2')
+        labels = {
+            'email': 'E-mail',
+            'first_name': 'Имя',
+            'last_name': 'Фамилия',
+            'roles': 'Роль',
+        }
+        # widgets = {
+        #     'email': forms.TextInput(attrs={'class': 'form-input'}),
+        #     'first_name': forms.TextInput(attrs={'class': 'form-input'}),
+        #     'last_name': forms.TextInput(attrs={'class': 'form-input'},),
+        # }
     # password = forms.CharField(label="Пароль", widget=forms.PasswordInput())
     # password2 = forms.CharField(label="Повтор пароля", widget=forms.PasswordInput())
     #
@@ -32,11 +51,6 @@ class RegisterUserForm(UserCreationForm):
     #         'first_name': 'Имя',
     #         'last_name': 'Фамилия',
     #     }
-    #     # widgets = {
-    #     #     'email': forms.TextInput(attrs={'class': 'form-input'}),
-    #     #     'first_name': forms.TextInput(attrs={'class': 'form-input'}),
-    #     #     'last_name': forms.TextInput(attrs={'class': 'form-input'}),
-    #     # }
     #
     # def clean_password2(self):
     #     cd = self.cleaned_data
@@ -44,8 +58,8 @@ class RegisterUserForm(UserCreationForm):
     #         raise forms.ValidationError("Пароли не совпадают!")
     #     return cd["password"]
     #
-    # def clean_email(self):
-    #     email = self.cleaned_data['email']
-    #     if get_user_model().objects.filter(email=email).exists():
-    #         raise forms.ValidationError("Такой E-mail уже существует")
-    #     return email
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if get_user_model().objects.filter(email=email).exists():
+            raise forms.ValidationError("Такой E-mail уже существует")
+        return email
